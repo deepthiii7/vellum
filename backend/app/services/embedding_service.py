7438f9b983f4cac70029bbe54c3aefab876e72f1
Maria Deepthi A <mariadeepthia@gmail.com>
2026-07-02 14:31:43 +0530

    embedding model

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -1,22 +1,10 @@
 from sentence_transformers import SentenceTransformer
 
-_model = None
-
-
-def get_model():
-    global _model
-
-    if _model is None:
-        _model = SentenceTransformer(
-            "all-MiniLM-L6-v2"
-        )
-
-    return _model
-
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)
 
 def generate_embeddings(chunks):
-
-    model = get_model()
 
     return model.encode(
         chunks,
